6dbd18d6efd2dee63fb6e92cd3832371edc80e2f
Lam <phatdatle374@gmail.com>
2024-10-20 17:10:06 +0700

    update

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const nameGirl = 'Lam';
 const giftUrl = 'http://nodemy.vn';
 const eventName = 'Chúc Mừng 20-10';
-const titleCard = 'Tặng người ấy';
+const titleCard = 'Tặng Lam';
 const contentCard = 'Chúc bạn Lam 20-10 vui vẻ. Mong điều đẹp nhất sẽ đến với m trong hôm nay và cả những ngày sau. Hôm nay t không tặng hoa m được nên có tí quà nho nhỏ tặng m nè. "Youtube Vip: "nngochoanglam@gmail.com|Hoanglamhacker"" "Photoshop full ALL: "atqlcynms@rustyload.com|SOCTRl!u32024"" ';
 
 // phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
